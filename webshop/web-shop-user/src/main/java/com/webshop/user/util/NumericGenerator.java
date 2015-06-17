b4882dffd793872726b2b1fda75a3c8ef6d7be27
psubhashreddy <p.subhashreddy@gmail.com>
2015-06-17 15:13:36 +0530

    added new file

---
+++ webshop/web-shop-user/src/main/java/com/webshop/user/util/NumericGenerator.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.webshop.user.util;
+
+/**
+ * @author speddyre
+ * @16th June 2015
+ *
+ */
+public final class NumericGenerator {
+
+	private static final String ALPHA_NUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	private NumericGenerator() {
+	}
+	
+	/**
+	 * This method is for generating the random alpha numeric no
+	 * @param int
+	 * @return String 
+	 */
+	public static String generateAlphaNumericNumeric() {
+		int count = 10;
+		StringBuilder builder = new StringBuilder();
+		while (count-- != 0) {
+			int character = (int) (Math.random() * ALPHA_NUMERIC
+					.length());
+			builder.append(ALPHA_NUMERIC.charAt(character));
+		}
+		return builder.toString();
+	}
+
+}
