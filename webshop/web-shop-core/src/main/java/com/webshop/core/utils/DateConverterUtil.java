228c027b2a8de237ff0807e5e067d6ee27b8a76a
psubhashreddy <p.subhashreddy@gmail.com>
2015-06-18 19:29:25 +0530

    Sprint 3 changes

--- webshop/web-shop-core/src/main/java/com/webshop/core/utils/DateConverterUtil.java
+++ webshop/web-shop-core/src/main/java/com/webshop/core/utils/DateConverterUtil.java
@@ -6,44 +6,51 @@ package com.webshop.core.utils;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
 
 /**
  * This is a util class for date converter from one type to other
  * 
  * @author speddyre
  * @date 5th June 2015
- *
  */
-public final class DateConverterUtil {
+public final class DateConverterUtil
+{
 
-	/**
-	 * Not to allow to instantiate it from outside
-	 */
-	private DateConverterUtil() {
+   /**
+    * Not to allow to instantiate it from outside
+    */
+   private DateConverterUtil()
+   {
 
-	}
+   }
 
-	/**
-	 * This method is used to convert the current java.util.Date to
-	 * java.sql.TimeStamp
-	 * 
-	 * @return TimeStamp
-	 */
-	public static Timestamp dateToTimeStamp() {
-		Date utilDate = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(utilDate);
-		cal.set(Calendar.MILLISECOND, 0);
-		return new Timestamp(utilDate.getTime());
+   /**
+    * This method is used to convert the current java.util.Date to java.sql.TimeStamp
+    * 
+    * @return TimeStamp
+    */
+   public static Timestamp dateToTimeStamp()
+   {
+      Date utilDate = new Date();
+      Calendar cal = Calendar.getInstance();
+      cal.setTime(utilDate);
+      cal.set(Calendar.MILLISECOND, 0);
+      return new Timestamp(utilDate.getTime());
 
-	}
-	
-	public static Timestamp getDateAfterDays(int noOfDays){
-		Date utilDate = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(utilDate);
-		cal.add(Calendar.DATE, noOfDays);
-		cal.set(Calendar.MILLISECOND, 0);
-		return new Timestamp(utilDate.getTime());
-	}
+   }
+
+   public static Timestamp getDateAfterDays(int noOfDays)
+   {
+      Date utilDate = new Date();
+      GregorianCalendar gregCalendar = new GregorianCalendar();
+      gregCalendar.setTime(utilDate);
+      gregCalendar.add(Calendar.DATE, noOfDays);
+      Date date = gregCalendar.getTime();
+      return new Timestamp(date.getTime());
+   }
+
 }
